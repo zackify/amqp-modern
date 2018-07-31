dc1d820da18f15edce42a0db19cbfba9c007f7df
Zach <zackify@gmail.com>
2018-07-31 11:25:34 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,37 @@
+## AMQP Modern
+
+A small `amqplib` wrapper for nodejs that simplified interactions with queue systems.
+
+- Consuming a channel
+- Async / Await based
+- Publishing to a channel
+- Passing off JS objects, so you don't have to
+- It handles rejections and acknowledgments automatically
+
+## Install
+
+```
+npm install amqp-modern
+```
+
+## Usage
+
+```js
+import amqp from 'amqp-modern';
+
+let client = amqp('amqp://connection string here');
+
+//consume a channel
+
+client.consume({
+  channel: 'message',
+  process: async message => {
+    await sendAnEmail(message.description);
+    //if this promise rejects, the queue will retry
+  },
+});
+
+//somewhere in your app, publish to a channel
+
+client.publish('message', { description: 'hello!' });
+```
---
+++ src/index.js
@@ -0,0 +1,37 @@
+export default url => {
+  const open = require('amqplib').connect(url);
+
+  const consume = async consumer => {
+    let connection = await open;
+    let ok = await connection.createChannel();
+    let channel = await ok;
+
+    channel.assertQueue(consumer.channel);
+
+    channel.consume(consumer.channel, async msg => {
+      if (!msg) return console.warn('empty message received');
+
+      try {
+        //if it fails to process, it will reject the message so it can be retried
+
+        let message = JSON.parse(msg.content.toString());
+        await consumer.process(message);
+        channel.ack(msg);
+      } catch (e) {
+        console.error(e);
+        channel.nack(msg);
+      }
+    });
+  };
+
+  const publish = async (name, message) => {
+    let connection = await open;
+    let ok = await connection.createChannel();
+    let channel = await ok;
+
+    channel.assertQueue(name);
+    channel.sendToQueue(name, new Buffer(JSON.stringify(message)));
+  };
+
+  return { consume, publish };
+};
